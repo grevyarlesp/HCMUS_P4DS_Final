b7a76f0ce545dd165f5e89887734f2cec89fa35a
grevyarlesp <tr4nquoc@gmail.com>
2022-01-10 00:38:23 +0700

    Add several things

---
+++ Notebook.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import sys
+sys.executable
+
+# %% [markdown]
+# # Thông tin: 
+#
+# ## Nhóm 11: 
+#
+# | MSSV     | Họ tên         | Github                                        |
+# |----------|----------------|-----------------------------------------------|
+# | 19127535 | Trần Kiến Quốc | [grevyarlesp](https://github.com/grevyarlesp) |
+# | 19127637 | Nguyễn Khắc Vỹ | [Khacvy1707](https://github.com/Khacvy1707)   |
+#
+# ## Các thư viện hỗ trợ
+
+# %%
+import pandas as pd
+
+# %% [markdown]
+# ## Collecting Data
+#
+# 1. What subject is your data about? What is the source of your data?
+#
+# Là tập dữ liệu chứa các câu trả lời của StackOverflow trong developer survey, cho 80000 người.  
+#
+# 2. Do authors of this data allow you to use like this? You can check the data license
+#
+# https://stackoverflow.blog/2021/08/30/the-full-data-set-for-the-2021-developer-survey-now-available/
+#
+# > Note you are free to share, adapt, and create derivative works from the public 2021 Stack Overflow Developer Survey results as long as you attribute them to Stack Overflow, keep the database open (if you redistribute it), and continue to share-alike any adapted database under the ODbL.
+#
+# 3.  How did authors collect data?
+#
+# Dữ liệu được thu thập bằng cách thực hiện khảo sát
+
+# %% [markdown]
+# ## Exploring Data
+
+# %% [markdown]
+# 1. How many rows and columns?
+
+# %% [markdown]
+# 2. What is the meaning of each row?
+
+# %% [markdown]
+# 3. Are there duplicated rows?
+#
+
+# %% [markdown]
+# 4. What is the meaning of each column?
+
+# %% [markdown]
+# 5. What is the current data type of each column? Are there columns having inappropriate data types?
+
+# %% [markdown]
+# 6.  With each numerical column, how are values distributed?
+#
+# -  What is the percentage of missing values?
+# -  Min? max? Are they abnormal?
+
+# %% [markdown]
+# 7. With each categorical column, how are values distributed?
+#
+# - What is the percentage of missing values?
+# - How many different values? Show a few. Are they abnormal?
+
+# %% [markdown]
+# ## Ask meaningful questions
+
+# %% [markdown]
+# ## Preprocessing data to answer the questions
+
+# %% [markdown]
+# ## Reflection
+
+# %% [markdown]
+# ## References
+
+# %% [markdown]
+#
+#
--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Programming 4 Data science course final project
 | 19127535 | Trần Kiến Quốc | [grevyarlesp](https://github.com/grevyarlesp) |
 | 19127637 | Nguyễn Khắc Vỹ | [Khacvy1707](https://github.com/Khacvy1707)   |
 
+# Tập dữ liệu:
+
+StackOverflow developer survey 2021
+
+https://insights.stackoverflow.com/survey
+
+# Note:
+
+Dùng [Jupytext](https://jupytext.readthedocs.io/en/latest/examples.html) để chuyển file, file .py thành dạng .ipynb, thuận tiện cho việc quản lí phiên bản qua Git.
+
+# Tracking:
+
+[Sheet](https://docs.google.com/spreadsheets/d/1Kav_E7HpoJ5g7HlPGOM7r1EVAatQpZZtShVN2gukFxI/edit?usp=sharing)
