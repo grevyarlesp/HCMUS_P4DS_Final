969c6eb55d3c11c7a56962e8f1e7804c92e271e1
grevyarlesp <tr4nquoc@gmail.com>
2022-01-15 21:31:17 +0700

    Add PL comments

--- Notebook.py
+++ Notebook.py
@@ -14,6 +14,8 @@
 #     name: python3
 # ---
 
+# %%
+
 # %% [markdown]
 # # Final Project
 
@@ -24,10 +26,12 @@ sys.executable
 # %% [markdown]
 # ## Group 11: 
 #
-# | MSSV     | Họ tên         | Github                                        |
-# |----------|----------------|-----------------------------------------------|
-# | 19127535 | Trần Kiến Quốc | [grevyarlesp](https://github.com/grevyarlesp) |
-# | 19127637 | Nguyễn Khắc Vỹ | [Khacvy1707](https://github.com/Khacvy1707)   |
+# | MSSV     | Họ tên         | Github                                        |Email                         | 
+# |----------|----------------|-----------------------------------------------|------------------------------|
+# | 19127535 | Trần Kiến Quốc | [grevyarlesp](https://github.com/grevyarlesp) |19127535@student.hcmus.edu.vn | 
+# | 19127637 | Nguyễn Khắc Vỹ | [Khacvy1707](https://github.com/Khacvy1707)   |nkvy19@clc.fitus.edu.vn       |
+#
+# Github Link: https://github.com/grevyarlesp/HCMUS_P4DS_Final/ 
 #
 # ## Dependencies
 
@@ -104,7 +108,7 @@ df1['OrgSize']
 #
 # Answers to the questions on stackoverflow. A few we may care about:
 #
-# - MainBranch
+# - MainBranch: type of developer (self employed, professional, ...) 
 # - Employment 
 # - Country 
 # - EdLevel : Education level
@@ -122,7 +126,7 @@ df1['OrgSize']
 # - OpSys: Their primary operating system
 # - Age: Age group they belong to
 # - NEWCollabToolsWantToWorkWith: developmenet environment (Vim,PyCharm, Notepad, Sublime,...)
-# - NEWCollabToolsHaveWorkedWith: developmenet environment (Vim,PyCharm, Notepad, Sublime,...)
+# - NEWCollabToolsHaveWorkedWith: development environment (Vim,PyCharm, Notepad, Sublime,...)
 # - Gender
 #
 # Refer to the questions sheet for more detail.
@@ -201,7 +205,7 @@ num_cols_info
 # - How many different values? Show a few. Are they abnormal?
 
 # %%
-df1[categorical_cols]
+df1[categorical_cols].head(10)
 
 # %%
 
@@ -212,29 +216,11 @@ cate_cols_info
 # ## Ask meaningful questions
 
 # %% [markdown]
-# 1. Top 10 countries with the highest average salaries. 
-#     
-#     **Benefits:** Salary is something that most working people are interested in and choosing the Top 10 countries with the           highest average salaries will help a lot in choosing a programmer's working environment.
-#     
-#     
+# 1. Top 10 countries with the highest average salaries.     
 # 2. Top 10 programming language.
-#
-#     **Benefits:** Programming languages are something that a programmer must know and the statistics of the Top 10 programming         languages will help those who are in the learning process can choose to learn the most popular programming languages.
-#     
-#     
 # 3. Most commonly used text editor / IDE for each operating system.
-#         
-#     **Benefits:** 
-#
-#
 # 4. Most common type of developer in Vietnam?
-#
-#     **Benefits:** Help to find out the most common types of developer in Vietnam and show how many of them have a Bachlelor's         degree.
-#
-#
 # 5. How long does it take for a programmer to become a pro?
-#     
-#     **Benefits:** People who are about to start learning programming are so interested in how long it will take them to become a       professional programmer and the answer to this question will help them know more about this.
 
 # %% [markdown]
 # ## Preprocessing data to answer the questions
@@ -244,6 +230,8 @@ cate_cols_info
 
 # %% [markdown]
 # #### 1. Top 10 countries with the highest average salaries. 
+#
+# Salary is something that most working people are interested in and choosing the Top 10 countries with the highest average salaries will help a lot in choosing a programmer's working environment.
 #
 # To answer this, we only consider countries with more than 1000 respondents. 
 #
@@ -259,21 +247,73 @@ s = s.head(10)
 
 # %%
 plt.figure()
-s['Average'].plot.barh()
+plt.title("Top 10 countries with the highest average salaries")
+s['Average'].plot.barh(color = 'green')
 
 # %% [markdown]
 # As expected, the US seems to have the highest compensation.
+#
+# The only representative of Asia is India, which proves that the resources for this industry in other Asian countries are still quite weak.
+#
+# European countries make up the majority of this ranking.
+#
+# If you intend to find another working environment abroad, the above countries are the top salary countries for you to choose.
 
 # %% [markdown]
 # #### 2. Top 10 programming language in 2021
-
-# %%
+#
+# Programming languages are something that a programmer must know and the statistics of the Top 10 programming languages will help those who are in the learning process can choose to learn the most popular programming languages.
 
 # %%
 lang_df = pd.DataFrame(df1['LanguageHaveWorkedWith'].str.split(';').explode().reset_index()).groupby('LanguageHaveWorkedWith').count()
 lang_df = lang_df.sort_values('index', ascending = False)
 lang_df.columns = ['count']
 display(lang_df.head(10))
+
+# %% [markdown]
+# The programming languages in the top 10 are all quite familiar and are used in popular industries such as software, AI, databases,...
+
+# %% [markdown]
+# Let's find out which language is the most popular amongst each type of developers. 
+
+# %%
+cols = ['MainBranch', 'LanguageHaveWorkedWith' ]
+lang_df2 = df1[cols]
+lang_df2.loc[:, cols[1]] = lang_df2[cols[1]].str.split(';').explode('LanguageHaveWorkedWith').dropna()
+ans_lang_df = lang_df2.groupby(cols).size().to_frame('Size').reset_index().pivot(index = cols[0], columns = cols[1], values = 'Size').fillna(0)
+display(ans_lang_df)
+
+for idx in ans_lang_df.index:
+    plt.figure()
+    plt.title(idx)
+    ans_lang_df.loc[idx].sort_values().plot.barh(color = 'lightblue')
+
+# %% [markdown]
+# Remarks: the histogram is quite similar in all categories, with JavaScript being a web scripting language at the top.
+#
+# Python is popular amongst students for its versatility.
+#
+# Most people know how to code Javascript, so if you want a job, then learn Javascript.
+
+# %% [markdown]
+# Similarly, let's look at the misc tech for each type of developer:
+
+# %%
+cols = ['MainBranch', 'MiscTechHaveWorkedWith' ]
+tech_df = df1[cols]
+tech_df.loc[:, cols[1]] = tech_df[cols[1]].str.split(';').explode('LanguageHaveWorkedWith').dropna()
+ans_tech_df = tech_df.groupby(cols).size().to_frame('Size').reset_index().pivot(index = cols[0], columns = cols[1], values = 'Size').fillna(0)
+display(ans_tech_df)
+
+for idx in ans_lang_df.index:
+    plt.figure()
+    plt.title(idx)
+    ans_tech_df.loc[idx].sort_values().plot.barh(color = 'lightblue')
+
+# %% [markdown]
+# NumPy is popular, because Python is popular. Pandas isn't as popular as Pandas for most cases.
+#
+# Despite Javascript popularity, React Native isn't more popular, surprisingly.
 
 # %% [markdown]
 # #### 3. Most commonly used text editor / IDE for each operating system.
@@ -292,15 +332,29 @@ tools_df = tools_df.pivot(columns = 'Tools', index = 'OpSys', values = 'Size')
 for idx in tools_df.index:
     plt.figure()
     plt.title(idx)
-    tools_df.loc[idx].sort_values().plot.barh()
+    tools_df.loc[idx].sort_values().plot.bar(color = 'purple')
+
+# %% [markdown]
+# VSCode is the most popular on most platforms. 
+#
+# Vim is more popular on BSD, suprisingly. There must be a correlation between using BSD and using Vim. 
+#
+# Windows users tend to use VS and VSCode more. 
 
 # %% [markdown]
 # #### 4. Most common type of developer in Vietnam.
+#
+# Finding out the most common types of developer in Vietnam.
 
 # %%
 vn_df = df1[df1['Country'] == 'Viet Nam']
-vn_df.groupby('MainBranch').size().plot.barh()
+plt.figure()
+plt.title("Most common type of developer in Vietnam.")
+vn_df.groupby('MainBranch').size().plot.barh(color = 'lightblue')
 
+
+# %% [markdown]
+# The results show that, in Vietnam, there are many different types of developers and not everyone needs formal training to be able to code.
 
 # %% [markdown]
 # By education level: How many developers in VN have a Bachlelor's degree?
@@ -308,15 +362,17 @@ vn_df.groupby('MainBranch').size().plot.barh()
 # %%
 s = 'I am a developer by profession'
 temp_df = vn_df[vn_df['MainBranch'] == s]
-temp_df.groupby('EdLevel').size().plot.barh()
+temp_df.groupby('EdLevel').size().plot.barh(color = 'orange')
 
 # %% [markdown]
 # So mostly they have a Bachelor's degree. 
 #
-# Dropping out is also feasible...
+# Because of the nature of this field, we can see that having a degree isn't necessary, but would make it easier. 
 
 # %% [markdown]
 # #### 5. How long does it take for a programmer to become a pro?
+#
+# People who are about to start learning programming are so interested in how long it will take them to become a       professional programmer and the answer to this question will help them know more about this.
 
 # %% [markdown]
 # To answer this question, we only use years for which figures are available.
@@ -331,6 +387,8 @@ time_take_df.mean().round(1)
 # It takes more than 5 years to become a pro programmer.
 #
 # The time taken is also relatively similar to the education time at universities and colleges.
+#
+# Statistical time shows that becoming a professional programmer does not take as long as people think. The difficulty of becoming a good programmer lies in the mindset, not the long, arduous training time.
 
 # %% [markdown] tags=[]
 # ## Reflection
